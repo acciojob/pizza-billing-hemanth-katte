b7e02ef2ddc78a14b650e5cad46bceca50c17c8b
Hemanth Katte <hemanthss24@gmail.com>
2022-12-12 09:41:16 +0530

    changed the delux pizza constructor

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -6,10 +6,10 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        if(isVeg)
-            super.price = 300;
-        else
-            super.price = 400;
+        //if(isVeg)
+           // super.price = 300;
+        //else
+           // super.price = 400;
         super.addExtraCheese();
         super.addExtraToppings();
 
